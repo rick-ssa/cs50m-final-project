57b1258e5cd0e94e543d4c9c0281065546eac9b3
Ricardo <rick_ssa@hotmail.com>
2020-02-26 20:53:23 -0300

    styling the scree search

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
 
 import SearchScreen from  './screens/SearchScreen'
 
+const Stack = createStackNavigator()
 
 export default function App() {
   
   
   return (
-    <View style={styles.container}>
-      <SearchScreen />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = 'home' component = {SearchScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: 'white',
     alignItems: 'stretch',
     justifyContent: 'center'
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -11,6 +11,7 @@ export default function SeachScreen(props) {
                 <TextInput 
                     style={styles.inputQuery} 
                     placeholder = 'Type your query'
+                    autoFocus 
                 />
 
             </View>
@@ -33,18 +34,23 @@ const styles = StyleSheet.create({
     },
 
     inputQueryWrap: {
-        height: 20,
+        flexDirection: 'row',
+        height: 50,
+        margin: 8,
+        marginTop: 15,
+        marginBottom: 15,
         borderColor: colors.DARKGREEN,
+        padding: 5,
+        borderWidth: 1,
         borderRadius: 5,
-        width: 300,
     },
 
-    /* inputQuery: {
-
+    inputQuery: {
+        flex: 1,
     },
 
     filterContainer: {
-
+        
     },
 
     searchButton: {
@@ -54,6 +60,6 @@ const styles = StyleSheet.create({
     textSearchButton: {
 
     }
- */
+
 
 })
