2f8aa5116d4ee3205c07bdd3a37e2fb775bdb401
Ricardo <rick_ssa@hotmail.com>
2020-02-26 20:31:32 -0300

    starded build search screen

--- App.js
+++ App.js
@@ -1,29 +1,15 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import colors from './assets/colors'
 
-import PillsContainer from './components/PillsContainer';
+import SearchScreen from  './screens/SearchScreen'
 
 
 export default function App() {
   
-  const [pills, setPills] = useState(
-    [{text:'Italian',onClose: 'hi', marginAround:4},{text:'Italian',onClose: 'hi', marginAround:4},{text:'Italian',onClose: 'hi', marginAround:4},{text:'Italian',onClose: 'hi', marginAround:4},{text:'Chinese',onClose: 'hi', marginAround:4}])
-  function handleCloseClick(i) {
-    setArr(()=>(arr.filter((v,index)=>{
-      if(index!==i) {
-        return true
-      }
-
-      return false
-    })))
-  }
+  
   return (
     <View style={styles.container}>
-     <PillsContainer pills = {pills} title='Cuisine' onAdd = {()=>console.log('you just did it')}/>
-     <PillsContainer pills = {pills.map(v=>{return v.text==='Italian'? {...v,text:'American'}:v})}title='Diet' onAdd = {()=>console.log('you just did it')}/>
-     <PillsContainer title='Intolerances' onAdd = {()=>console.log('you just did it')}/>
-     <PillsContainer title='Excluded Ingredients' onAdd = {()=>console.log('you just did it')}/>
+      <SearchScreen />
     </View>
   );
 }
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,2 +1,59 @@
 import React from 'react'
-import {View, Text, TouchableOpacity} from 'react-native'
+import {View, Text,TextInput, TouchableOpacity,ScrollView, StyleSheet} from 'react-native'
+import PillsContainer from '../components/PillsContainer'
+import colors from '../assets/colors'
+
+export default function SeachScreen(props) {
+    return (
+        <View  style={styles.container}>
+            <View style={styles.inputQueryWrap}>
+
+                <TextInput 
+                    style={styles.inputQuery} 
+                    placeholder = 'Type your query'
+                />
+
+            </View>
+            <ScrollView style={styles.filterContainer}>
+                <PillsContainer />
+                <PillsContainer />
+                <PillsContainer />
+                <PillsContainer />
+            </ScrollView>
+            <TouchableOpacity style={styles.searchButton}>
+                <Text style={styles.textSearchButton}></Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "column",
+    },
+
+    inputQueryWrap: {
+        height: 20,
+        borderColor: colors.DARKGREEN,
+        borderRadius: 5,
+        width: 300,
+    },
+
+    /* inputQuery: {
+
+    },
+
+    filterContainer: {
+
+    },
+
+    searchButton: {
+
+    },
+
+    textSearchButton: {
+
+    }
+ */
+
+})
